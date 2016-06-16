28a07b5e15790381a6adc0a0aaa10deb9e408018
Kevin Duraj <kduraj@gmail.com>
2016-06-16 13:04:16 -0700

    2016-06-16 13:04

--- install-centos-dse48.sh
+++ install-centos-dse48.sh
@@ -6,6 +6,7 @@ export EMAIL='kduraj%40gmail.com'
 export PRIVATE_IP=$(hostname -I | awk '{print $1}')
 #----------------------------------------------------------------------------------------------------------#
 declare -a node3=('-9223372036854775808', '-3074457345618258603', '3074457345618258602')
+declare -a node5=('-9223372036854775808', '-5534023222112865485', '-1844674407370955162', '1844674407370955161', '5534023222112865484')
 #----------------------------------------------------------------------------------------------------------#
 # Default Settings:
 #----------------------------------------------------------------------------------------------------------#
@@ -39,7 +40,7 @@ sed -i 's/#HEAP_NEWSIZE="800M"/HEAP_NEWSIZE="1G"/' /etc/dse/cassandra/cassandra-
 #----------------------------------------------------------------------------------------------------------#
 sed -i 's/SPARK_ENABLED=0/SPARK_ENABLED=1/' /etc/default/dse
 sed -i "s/Test Cluster/$cluster/" /etc/dse/cassandra/cassandra.yaml
-token=${node3[$NODE_NUMBER]}
+token=${node5[$NODE_NUMBER]}
 sed -i "s/# initial_token:/initial_token: $token/" /etc/dse/cassandra/cassandra.yaml
 sed -i "s/127.0.0.1/$seeds/" /etc/dse/cassandra/cassandra.yaml
 sed -i 's/com.datastax.bdp.snitch.DseSimpleSnitch/GossipingPropertyFileSnitch/' /etc/dse/cassandra/cassandra.yaml
