a74a75ab91d589f30ac5f54ccb6f62cb82e553b8
Kevin Duraj <kduraj@gmail.com>
2016-06-19 18:15:44 -0700

    2016-06-19 18:15

--- cluster.sh
+++ cluster.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 #---------------------------------------------------------#
 ACTION='stop'
-#---------------------------------------------------------#
 if [ $# -eq 0 ]; then
-   echo "Starting cluster"
    ACTION='start'
-   sleep 1
-else 
-   echo "Cluster ${ACTION}"
-   sleep 1;
-
 fi
-
 #---------------------------------------------------------#
 for node in 192.168.1.133 192.168.1.132 192.168.1.159;
 do
-  echo $node
+  echo "Node: ${node} ${ACTION}"
   ssh $node service dse ${ACTION} 
 done
 
