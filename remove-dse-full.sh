9cbed710c8786a03c6fce8380c526fda0beb4276
Kevin Duraj <kduraj@gmail.com>
2016-07-05 13:28:15 -0700

    Update remove-dse-full.sh

--- remove-dse-full.sh
+++ remove-dse-full.sh
@@ -8,5 +8,9 @@ rm -fR /usr/share/doc/dse-lib*
 rm -fR /usr/share/opscenter
 rm -fR /etc/opscenter
 rm -f /etc/rc.d/rc0.d/*dse
+#-------------------------------------------------------------------------------------------#
+# Remove Services for DSE 5.0
+#-------------------------------------------------------------------------------------------#
 rm -f /etc/rc.d/rc1.d/K50dse /etc/rc.d/rc1.d/K05opscenterd /etc/rc.d/rc1.d/K05datastax-agent
+rm -f /etc/rc.d/rc2.d/S50dse /etc/rc.d/rc2.d/S80datastax-agent /etc/rc.d/rc2.d/S80opscenterd
 #-------------------------------------------------------------------------------------------#
