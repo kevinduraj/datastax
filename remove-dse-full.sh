5bef68a87d61b5cbca68f2e9604ae2d48be672fa
Kevin Duraj <kduraj@gmail.com>
2016-07-05 13:29:43 -0700

    Update remove-dse-full.sh

--- remove-dse-full.sh
+++ remove-dse-full.sh
@@ -13,4 +13,5 @@ rm -f /etc/rc.d/rc0.d/*dse
 #-------------------------------------------------------------------------------------------#
 rm -f /etc/rc.d/rc1.d/K50dse /etc/rc.d/rc1.d/K05opscenterd /etc/rc.d/rc1.d/K05datastax-agent
 rm -f /etc/rc.d/rc2.d/S50dse /etc/rc.d/rc2.d/S80datastax-agent /etc/rc.d/rc2.d/S80opscenterd
+rm -f /etc/rc.d/rc3.d/S50dse /etc/rc.d/rc3.d/S80datastax-agent /etc/rc.d/rc3.d/S80opscenterd
 #-------------------------------------------------------------------------------------------#
