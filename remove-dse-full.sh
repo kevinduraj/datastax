2404e60bc6c342c92bedf3bdb229a213a870c1ed
Kevin Duraj <kduraj@gmail.com>
2016-07-05 13:23:04 -0700

    Update remove-dse-full.sh

--- remove-dse-full.sh
+++ remove-dse-full.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #-------------------------------------------------------------------#
 yum -y remove dse
+rm -fR /etc/dse
 rm -fR /usr/share/dse
 rm -fR /usr/share/doc/dse-lib*
 rm -fR /usr/share/opscenter
 rm -fR /etc/opscenter
+
