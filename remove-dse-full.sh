25969176059a0f06fe1660f1c4532e30ba50f27f
Kevin Duraj <kduraj@gmail.com>
2016-07-05 13:38:45 -0700

    Update remove-dse-full.sh

--- remove-dse-full.sh
+++ remove-dse-full.sh
@@ -29,3 +29,4 @@ rm -f /etc/rc.d/rc4.d/S50dse /etc/rc.d/rc4.d/S80datastax-agent /etc/rc.d/rc4.d/S
 rm -f /etc/rc.d/rc5.d/S50dse /etc/rc.d/rc5.d/S80datastax-agent /etc/rc.d/rc5.d/S80opscenterd
 rm -f /etc/rc.d/rc6.d/K50dse /etc/rc.d/rc6.d/K05datastax-agent /etc/rc.d/rc6.d/K05opscenterd
 #-------------------------------------------------------------------------------------------#
+rm -fR /var/cache/yum/x86_64/7/datastax
