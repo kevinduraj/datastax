dd7b752348071688511f35cf29775db3ffc3b9c3
Kevin Duraj <kduraj@gmail.com>
2016-07-05 13:26:22 -0700

    Update remove-dse-full.sh

--- remove-dse-full.sh
+++ remove-dse-full.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#-------------------------------------------------------------------#
+#-------------------------------------------------------------------------------------------#
 yum -y remove dse
 rm -fR /etc/dse
 rm -fR /usr/share/dse
@@ -8,4 +8,5 @@ rm -fR /usr/share/doc/dse-lib*
 rm -fR /usr/share/opscenter
 rm -fR /etc/opscenter
 rm -f /etc/rc.d/rc0.d/*dse
-
+rm -f /etc/rc.d/rc1.d/K50dse /etc/rc.d/rc1.d/K05opscenterd /etc/rc.d/rc1.d/K05datastax-agent
+#-------------------------------------------------------------------------------------------#
